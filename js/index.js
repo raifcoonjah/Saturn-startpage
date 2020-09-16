20c591f3c9343929f540e33dfb795b6ab4293506
mraif13 <raifms1200@gmail.com>
2020-09-16 21:49:04 +0400

    removed :boom: Removed weather
    :alembic: Removed weather widget, you can renable it if you want, the code is still there.
    :alembic: Bloc in modal got a slight recoloring making it easier on the eyes.
    :bug: The modal is still super buggy and probably will get replaced with something else soon.
    :zap: Slight performance boost from previous versions.
    
    Signed-off-by: mraif13 <raifms1200@gmail.com>

--- js/index.js
+++ js/index.js
@@ -55,43 +55,43 @@ function getDate() {
   return " " + satday + " " + satnum + " " + satmonth;
 }
 // Weather system, add your api key here
-window.onload = () => {
-  let xhr = new XMLHttpRequest();
-  // Request to open weather map
-  xhr.open(
-    "GET",
-    "https://api.openweathermap.org/data/2.5/weather?id=934154&&units=metric&appid=APIKEY"
-  );
-  xhr.onload = () => {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        var prefix = "wi-";
-        var today = new Date();
-        var hour = today.getHours();
-        var dn = "";
-        if (hour > 5 && hour < 19) {
-          dn = "";
-        } else {
-          dn = "night-";
-        }
-        let json = JSON.parse(xhr.responseText);
-        console.log(json);
-        var code = json.weather[0].id;
-        wIcon = prefix + "owm-" + dn + code;
-        console.log(wIcon);
-        document.getElementById("temp").innerHTML =
-          json.main.temp.toFixed(0) + "°";
-        document.getElementById("weather-icon").classList.add(wIcon);
-      } else {
-        console.log("error msg: " + xhr.status);
-      }
-    }
-  };
-  xhr.send();
-  // Set up the clock and date
-  document.getElementById("time").innerHTML =
-    `It's ` + getDate() + `, ` + getTime();
-};
+// window.onload = () => {
+//   let xhr = new XMLHttpRequest();
+//   // Request to open weather map
+//   xhr.open(
+//     "GET",
+//     "https://api.openweathermap.org/data/2.5/weather?id=934154&&units=metric&appid=APIKEY"
+//   );
+//   xhr.onload = () => {
+//     if (xhr.readyState === 4) {
+//       if (xhr.status === 200) {
+//         var prefix = "wi-";
+//         var today = new Date();
+//         var hour = today.getHours();
+//         var dn = "";
+//         if (hour > 5 && hour < 19) {
+//           dn = "";
+//         } else {
+//           dn = "night-";
+//         }
+//         let json = JSON.parse(xhr.responseText);
+//         console.log(json);
+//         var code = json.weather[0].id;
+//         wIcon = prefix + "owm-" + dn + code;
+//         console.log(wIcon);
+//         document.getElementById("temp").innerHTML =
+//           json.main.temp.toFixed(0) + "°";
+//         document.getElementById("weather-icon").classList.add(wIcon);
+//       } else {
+//         console.log("error msg: " + xhr.status);
+//       }
+//     }
+//   };
+//   xhr.send();
+// Set up the clock and date
+document.getElementById("time").innerHTML =
+  `It's ` + getDate() + `, ` + getTime();
+// };
 
 // Settings/Modal trigger + close button.
 var modal = document.querySelector(".modal");
