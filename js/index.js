604e0a8c6949fb35933f06bb3dfc51e967166b05
mraif13 <raifms1200@gmail.com>
2020-09-17 23:16:11 +0400

    feat: :zap: Improved performance
    - Improved modal slightly by fixing some bugs.
    - Added myself x)
    - Added updates.json to handle all updates for saturn.
    - Updated manifest.json
    - Updated styles.css
    :bug: A bug has been noticed on firefox when opening a new-tab
    
    Signed-off-by: mraif13 <raifms1200@gmail.com>

--- js/index.js
+++ js/index.js
@@ -54,40 +54,6 @@ function getDate() {
     satnum = date.getDate();
   return " " + satday + " " + satnum + " " + satmonth;
 }
-// Weather system, add your api key here
-// window.onload = () => {
-//   let xhr = new XMLHttpRequest();
-//   // Request to open weather map
-//   xhr.open(
-//     "GET",
-//     "https://api.openweathermap.org/data/2.5/weather?id=934154&&units=metric&appid=APIKEY"
-//   );
-//   xhr.onload = () => {
-//     if (xhr.readyState === 4) {
-//       if (xhr.status === 200) {
-//         var prefix = "wi-";
-//         var today = new Date();
-//         var hour = today.getHours();
-//         var dn = "";
-//         if (hour > 5 && hour < 19) {
-//           dn = "";
-//         } else {
-//           dn = "night-";
-//         }
-//         let json = JSON.parse(xhr.responseText);
-//         console.log(json);
-//         var code = json.weather[0].id;
-//         wIcon = prefix + "owm-" + dn + code;
-//         console.log(wIcon);
-//         document.getElementById("temp").innerHTML =
-//           json.main.temp.toFixed(0) + "°";
-//         document.getElementById("weather-icon").classList.add(wIcon);
-//       } else {
-//         console.log("error msg: " + xhr.status);
-//       }
-//     }
-//   };
-//   xhr.send();
 // Set up the clock and date
 document.getElementById("time").innerHTML =
   `It's ` + getDate() + `, ` + getTime();
@@ -111,29 +77,3 @@ function windowOnClick(event) {
 trigger.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
-
-// Dark mode Switch. Light > Dark | Dark < light
-const toggleSwitch = document.querySelector(
-  '.theme-switch input[type="checkbox"]'
-);
-const currentTheme = localStorage.getItem("theme");
-
-if (currentTheme) {
-  document.documentElement.setAttribute("data-theme", currentTheme);
-
-  if (currentTheme === "dark") {
-    toggleSwitch.checked = true;
-  }
-}
-
-function switchTheme(e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark");
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-    localStorage.setItem("theme", "light");
-  }
-}
-
-toggleSwitch.addEventListener("change", switchTheme, false);
