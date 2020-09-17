e316f26643520803e8b603439c5aaf423fbb7ad9
mraif13 <raifms1200@gmail.com>
2020-09-18 01:19:35 +0400

    feat: :sparkles: v2.1_release-candit1
    perf: Simplied image rotator code.
    perf: Removed useless lines of code.
    perf!: Removed weather widget completely.
    feat: Add 20 brand new art works.
    feat: Simplified version naming scheme.
    feat: Add a pretty neat looking gradient effect on Good morning text.
    feat: Add myself.
    feat(fix): Modal has been simplied with a more simple design and animation.
    feat: Add brand new section for misc in modal.
    refactor: Add [BETA] tag near version number.
    
    Signed-off-by: mraif13 <raifms1200@gmail.com>

--- js/Image-rotator.js
+++ js/Image-rotator.js
@@ -1,24 +1,31 @@
-// Uses javascript to switch between 3 different images. 
-// v0.1.2
+// Uses javascript to switch between 3 different images.
+// v0.2
 
-// Rotate Images using Mafs.
-var theImages = new Array();
-theImages[0] = "./img/1-min.png";
-theImages[1] = "./img/2-min.png";
-theImages[2] = "./img/3-min.png";
-var j = 0;
-var p = theImages.length;
-var preBuffer = new Array();
+var images = [
+  "./img/1.svg",
+  "./img/2.svg",
+  "./img/3.svg",
+  "./img/4.svg",
+  "./img/6.svg",
+  "./img/7.svg",
+  "./img/8.svg",
+  "./img/10.svg",
+  "./img/11.svg",
+  "./img/12.svg",
+  "./img/14.svg",
+  "./img/15.svg",
+  "./img/16.svg",
+  "./img/17.svg",
+  "./img/18.svg",
+  "./img/19.svg",
+  "./img/20.svg",
+  "./img/21.svg",
+];
 
-for (i = 1; i < p; i++) {
-  preBuffer[i] = new Image();
-  preBuffer[i].src = theImages[i];
+function randImg() {
+  var size = images.length;
+  var x = Math.floor(size * Math.random());
+  document.getElementById("headerimg").src = images[x];
 }
-var whichImage = Math.round(Math.random() * (p - 1));
 
-function showImage() {
-  document.getElementById("headerimg").src = theImages[whichImage];
-}
-
-//call the function
-showImage();
+randImg();
--- js/custom.js
+++ js/custom.js
@@ -40,40 +40,6 @@ function getDate() {
     cnum = date.getDate();
   return " " + cday + " " + cnum + " " + cmonth;
 }
-window.onload = () => {
-  let xhr = new XMLHttpRequest();
-  // Request to open weather map
-  xhr.open(
-    "GET",
-    "https://api.openweathermap.org/data/2.5/weather?id=934154&&units=metric&appid=853f035d7995af09f2ca63841a1f5e76"
-  );
-  xhr.onload = () => {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        var prefix = "wi-";
-        var today = new Date();
-        var hour = today.getHours();
-        var dn = "";
-        if (hour > 5 && hour < 19) {
-          dn = "";
-        } else {
-          dn = "night-";
-        }
-        let json = JSON.parse(xhr.responseText);
-        console.log(json);
-        var code = json.weather[0].id;
-        wIcon = prefix + "owm-" + dn + code;
-        console.log(wIcon);
-        document.getElementById("temp").innerHTML =
-          json.main.temp.toFixed(0) + "°";
-        document.getElementById("weather-icon").classList.add(wIcon);
-      } else {
-        console.log("error msg: " + xhr.status);
-      }
-    }
-  };
-  xhr.send();
-  // Set up the clock and date
-  document.getElementById("time").innerHTML =
-    `It's ` + getDate() + `, ` + getTime();
-};
+// Set up the clock and date
+document.getElementById("time").innerHTML =
+  `It's ` + getDate() + `, ` + getTime();
