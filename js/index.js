0dd1c1d1317f8e9ecf79a4079d8738f84e382084
mraif13 <raifms1200@gmail.com>
2020-09-18 13:51:27 +0400

    perf: :zap: Removed useless stuff
    feat: Add updates.json
    refactor: Removed old images.
    refactor: Move time from custom.js to index.js
    Signed-off-by: mraif13 <raifms1200@gmail.com>

--- js/index.js
+++ js/index.js
@@ -58,3 +58,48 @@ function switchTheme(e) {
 }
 
 toggleSwitch.addEventListener("change", switchTheme, false);
+
+// Time v2.0
+function getTime() {
+  let date = new Date(),
+    min = date.getMinutes(),
+    //sec = date.getSeconds(),
+    hour = date.getHours();
+  return (
+    "" + (hour < 10 ? "0" + hour : hour) + ":" + (min < 10 ? "0" + min : min)
+  ); // + ":" +
+  //(sec < 10 ? ("0" + sec) : sec);
+}
+function getDate() {
+  let date = new Date(),
+    months = [
+      "Jan",
+      "Febr",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    cmonth = months[date.getMonth()],
+    days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ],
+    cday = days[date.getDay()],
+    cnum = date.getDate();
+  return " " + cday + " " + cnum + " " + cmonth;
+}
+// Set up the clock and date
+document.getElementById("time").innerHTML =
+  `It's ` + getDate() + `, ` + getTime();
