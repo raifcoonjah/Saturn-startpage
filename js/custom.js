c730bd0a0376f19b5b8307e119961550652c6e7e
mraif13 <raifms1200@gmail.com>
2020-09-20 21:17:38 +0400

    feat: :alembic: Add Saturn dash dash, a more efficient way to add more apps to saturn
    Signed-off-by: mraif13 <raifms1200@gmail.com>

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,9 @@
 // Add your custom js stuff here
 // Want to contribute to saturn startpage? Learn more on our github page.
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
