c7dcee6bf5b467b0e59d4cfc6074c5b191cb9a4d
mraif13 <raifms1200@gmail.com>
2020-09-28 17:09:58 +0400

    feat: :art: Add solarized theme [ALPHA TEST]
    Signed-off-by: mraif13 <raifms1200@gmail.com>

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,65 @@
 // Add your custom js stuff here
 // Want to contribute to saturn startpage? Learn more on our github page.
+// DOM Elements
+
+const darkButton = document.getElementById("dark");
+const lightButton = document.getElementById("light");
+const solarButton = document.getElementById("solar");
+const material_darkButton = document.getElementById("material-dark");
+const body = document.body;
+
+// Apply the cached theme on reload
+
+const theme = localStorage.getItem("theme");
+const isSolar = localStorage.getItem("isSolar");
+
+if (theme) {
+  body.classList.add(theme);
+  isSolar && body.classList.add("solar");
+}
+
+// Button Event Handlers
+
+darkButton.onclick = () => {
+  body.classList.replace("light", "dark");
+  localStorage.setItem("theme", "dark");
+};
+
+lightButton.onclick = () => {
+  body.classList.replace("dark", "light");
+
+  localStorage.setItem("theme", "light");
+};
+
+solarButton.onclick = () => {
+  if (body.classList.contains("solar")) {
+    body.classList.remove("solar");
+    solarButton.style.cssText = `
+      --bg-solar: var(--yellow);
+    `;
+
+    solarButton.innerText = "solarize";
+
+    localStorage.removeItem("isSolar");
+  } else {
+    solarButton.style.cssText = `
+      --bg-solar: white;
+    `;
+
+    body.classList.add("solar");
+    solarButton.innerText = "normalize";
+
+    body.classList.add("solar");
+    solarButton.innerText = "solar";
+
+    localStorage.setItem("ismat-dark", true);
+  }
+};
+if (theme) {
+  body.classList.add(theme);
+  isSolar && body.classList.add("solar");
+  if (isSolar) {
+    solarButton.innerText = "Normalize";
+    solarButton.style.cssText = `--bg-solar: white`;
+  }
+}
