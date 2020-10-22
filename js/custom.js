963d03fea29a141ef3a4364a59df0aac32201630
mraif13 <raifms1200@gmail.com>
2020-10-23 01:33:18 +0400

    feat: :lipstick: Add reddit section, updated manifest.json content policy.
    - Beta release, new features are unstable.
    - Added a brand new Shortcuts hover animation.
    - Added reddit section with most visited subreddits.
    - Other minor improvements.
    
    Signed-off-by: mraif13 <raifms1200@gmail.com>

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,15 @@
-// Add your custom js stuff here
-// Want to contribute to saturn startpage? Learn more on our github page.
+function show_more() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("btn_shmore");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
