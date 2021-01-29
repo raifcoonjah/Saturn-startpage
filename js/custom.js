59fff5f51d7c55b578e8b11c73ea251dd023159d
mraif13 <raifms1200@gmail.com>
2021-01-29 16:07:14 +0400

    feat: :sparkles: Add ability to hide one apps list row
    Signed-off-by: mraif13 <raifms1200@gmail.com>

--- js/custom.js
+++ js/custom.js
@@ -41,3 +41,22 @@ $(function () {
     localStorage.setItem("favorites_hidden", this.checked);
   });
 });
+
+$(function () {
+  var status = localStorage.getItem("apps_row2_hidden");
+  if (status == "true") {
+    $(".apps_row2").css("display", "none");
+    $(".AvGamesCheckBox_apps_row2").attr("checked", true);
+  } else {
+    $(".apps_row2").css("display", "block");
+    $(".AvGamesCheckBox_apps_row2").attr("checked", false);
+  }
+  $(".AvGamesCheckBox_apps_row2").click(function () {
+    if (this.checked) {
+      $(".apps_row2").hide();
+    } else {
+      $(".apps_row2").show();
+    }
+    localStorage.setItem("apps_row2_hidden", this.checked);
+  });
+});
