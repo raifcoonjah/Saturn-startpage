74439ca070272b1e438c15562d25832756e1a379
mraif13 <raifms1200@gmail.com>
2021-01-30 11:47:19 +0400

    fix: :bug: v2.4.0.1
    Signed-off-by: mraif13 <raifms1200@gmail.com>

--- js/custom.js
+++ js/custom.js
@@ -60,3 +60,22 @@ $(function () {
     localStorage.setItem("apps_row2_hidden", this.checked);
   });
 });
+
+// // favorites
+// // Search v1.0
+// function favSearchFunction() {
+//   var input, filter, ul, li, a, i, txtValue;
+//   input = document.getElementById("favSearch");
+//   filter = input.value.toUpperCase();
+//   ul = document.getElementById("fav-link");
+//   li = ul.getElementsByTagName("li");
+//   for (i = 0; i < li.length; i++) {
+//     a = li[i].getElementsByTagName("a")[0];
+//     txtValue = a.textContent || a.innerText;
+//     if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//       li[i].style.display = "";
+//     } else {
+//       li[i].style.display = "none";
+//     }
+//   }
+// }
