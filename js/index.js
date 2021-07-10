550724bbbc9f950b9f4676204e5ea4d9defe9654
Raif Coonjah <raifcoonjah@pm.me>
2021-07-10 16:34:04 +0400

    fix: :sparkles: New theme "OH purple"
    - Added new theme called "OH purple"
    - Fix various css issues introduced with new css theme.
    - Add new blur image.
    - SEVERAL css ajustments thoughout.
    - Added new images.
    - Bug fixes.
    
    Signed-off-by: Raif Coonjah <raifcoonjah@pm.me>

--- js/index.js
+++ js/index.js
@@ -152,6 +152,7 @@ const darkButton = document.getElementById("dark");
 const lightButton = document.getElementById("light");
 const spaceButton = document.getElementById("space");
 const linkinParkButton = document.getElementById("linkin-park");
+const ohpurpleButton = document.getElementById("purple");
 const body = document.body;
 
 // Apply the cached theme on reload
@@ -168,6 +169,7 @@ darkButton.onclick = () => {
   body.classList.replace("light", "dark");
   body.classList.replace("space", "dark");
   body.classList.replace("linkin-park", "dark");
+  body.classList.replace("purple", "dark");
   localStorage.setItem("theme", "dark");
 };
 
@@ -175,6 +177,7 @@ lightButton.onclick = () => {
   body.classList.replace("dark", "light");
   body.classList.replace("space", "light");
   body.classList.replace("linkin-park", "light");
+  body.classList.replace("purple", "light");
   localStorage.setItem("theme", "light");
 };
 
@@ -182,6 +185,7 @@ spaceButton.onclick = () => {
   body.classList.replace("dark", "space");
   body.classList.replace("light", "space");
   body.classList.replace("linkin-park", "space");
+  body.classList.replace("purple", "space");
   localStorage.setItem("theme", "space");
 };
 
@@ -189,5 +193,14 @@ linkinParkButton.onclick = () => {
   body.classList.replace("dark", "linkin-park");
   body.classList.replace("light", "linkin-park");
   body.classList.replace("space", "linkin-park");
+  body.classList.replace("purple", "linkin-park");
   localStorage.setItem("theme", "linkin-park");
+};
+
+ohpurpleButton.onclick = () => {
+  body.classList.replace("dark", "purple");
+  body.classList.replace("light", "purple");
+  body.classList.replace("space", "purple");
+  body.classList.replace("linkin-park", "purple");
+  localStorage.setItem("theme", "purple");
 };
