b87c2bc9b5bd5a1e82767c824c11559f294f96e1
Raif Coonjah <raifcoonjah@pm.me>
2021-09-04 13:45:47 +0400

    fix: :bug: Fix wrong border color
    - Added more apps button back.
    
    Signed-off-by: Raif Coonjah <raifcoonjah@pm.me>

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,13 @@
 // Add Javascript here.
 // Think this should be in Saturn Startpage? Feel free to make a PR.
+// Open overlay menu
+navy.addEventListener("click", openNav);
+navyclose.addEventListener("click", closeNav);
+
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
