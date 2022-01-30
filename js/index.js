cfbc9310ed2159d1d0bfab0ed74942b090c82266
Raif Coonjah <raifcoonjah@pm.me>
2022-01-30 11:52:51 +0400

    favorites input
    * Favorites input will automatically have the "https://" in the URL input.
    * Remove the ability to hide apps list.
    * Save button redesign has been reverted but with a darker and "thicc" border.
    
    Signed-off-by: Raif Coonjah <raifcoonjah@pm.me>

--- js/index.js
+++ js/index.js
@@ -139,42 +139,24 @@ $(function () {
 });
 
 $(function () {
-  var status = localStorage.getItem("apps_hidden");
+  var status = localStorage.getItem("hide-calendar-btn");
   if (status == "true") {
-    $(".apps_list").css("display", "none");
+    $(".calendar-btn").css("display", "none");
     $(".hide-apps-list").attr("checked", true);
   } else {
-    $(".apps_list").css("display", "block");
-    $(".hide-apps-list").attr("checked", false);
+    $(".calendar-btn").css("display", "block");
+    $(".hide-calendar-btn").attr("checked", false);
   }
-  $(".hide-apps-list").click(function () {
+  $(".hide-calendar-btn").click(function () {
     if (this.checked) {
-      $(".apps_list").hide();
+      $(".calendar-btn").hide();
     } else {
-      $(".apps_list").show();
+      $(".calendar-btn").show();
     }
-    localStorage.setItem("hide-apps-list", this.checked);
-  });
-
-  $(function () {
-    var status = localStorage.getItem("hide-calendar-btn");
-    if (status == "true") {
-      $(".calendar-btn").css("display", "none");
-      $(".hide-apps-list").attr("checked", true);
-    } else {
-      $(".calendar-btn").css("display", "block");
-      $(".hide-calendar-btn").attr("checked", false);
-    }
-    $(".hide-calendar-btn").click(function () {
-      if (this.checked) {
-        $(".calendar-btn").hide();
-      } else {
-        $(".calendar-btn").show();
-      }
-      localStorage.setItem("hide-calendar-btn", this.checked);
-    });
+    localStorage.setItem("hide-calendar-btn", this.checked);
   });
 });
+
 //
 // ========
 // + Settings navigation +
