14a5937622734da0fe9214b51adafc55ffa4b64a
Raif Coonjah <raifcoonjah@pm.me>
2022-03-05 15:11:29 +0400

    fix: Fix a major issue that made it hard to see text typed inside the searchbar
    Signed-off-by: Raif Coonjah <raifcoonjah@pm.me>

--- js/render.js
+++ js/render.js
@@ -160,7 +160,7 @@ const settingAbout = (z) => {
                 <a
                   href="https://github.com/mraif13/Saturn-startpage/releases/tag/v5.0.0"
                   target="_blank" 
-                  >v5.0.0 Dev <i class="las la-external-link-alt"></i
+                  >v5.0.0 <i class="las la-external-link-alt"></i
                 ></a>
                 <br />
                 Help make saturn better
