3841f04adc8948dd126bbf3eaa0aaf464a47c2ae
Raif Coonjah <raifcoonjah@pm.me>
2022-03-22 10:00:44 +0400

    Create dynamic-apps.js

---
+++ dynamic-apps.js
@@ -0,0 +1,8 @@
+//
+// ========
+// + Dynamic apps - Saturn Startpage +
+// + Last updated on: 3/22/22 +
+// + Apps here are dynamic and can be added or removed from the list without updates. +
+// + A PR is required for every addition. +
+// ========
+//
