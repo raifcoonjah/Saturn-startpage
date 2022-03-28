31661fb6a0bb2e5efa6faf32a19109e86f3d22ed
Raif Coonjah <raifcoonjah@pm.me>
2022-03-28 13:54:33 +0400

    fix: Revert to testing version
    * Include proper build number.
    
    Signed-off-by: Raif Coonjah <raifcoonjah@pm.me>

--- js/render.js
+++ js/render.js
@@ -159,7 +159,7 @@ const settingAbout = (z) => {
                 Saturn Desktop: <a
                   href="https://github.com/mraif13/Saturn-startpage/releases/tag/v5.0.0"
                   target="_blank" 
-                  >v5.0.0 <i class="las la-external-link-alt"></i
+                  >v5.0.0 | Build 4.9.28 <i class="las la-external-link-alt"></i
                 ></a>
                 <br />
                 Help make saturn better
