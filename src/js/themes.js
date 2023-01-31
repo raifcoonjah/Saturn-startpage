59ae69f3dc0a5da0a8e046c31a2c199afa9ea8c0
Raif Coonjah <raifcoonjah@proton.me>
2023-01-31 06:56:53 +0400

    feat: Smooth theme transitions without bugs!
    * fix border and outline conflict
    
    Signed-off-by: Raif Coonjah <raifcoonjah@proton.me>

--- src/js/themes.js
+++ src/js/themes.js
@@ -36,13 +36,14 @@ const setTheme = (theme) => {
   theme_choice.forEach((button) => (button.style.outline = ""));
   document.getElementById(theme).style.outline =
     "4px solid var(--bg-accent-color-lr-hover)";
+  document.body.style.transition = "0.3s ease-in";
 };
 
 const themeApplied = localStorage.getItem("theme");
 if (themeApplied) {
   body.classList.add(themeApplied);
   document.theme_choice;
-  document.getElementById(themeApplied).style.border =
+  document.getElementById(themeApplied).style.outline =
     "4px solid var(--bg-accent-color-lr)";
 } else {
   body.classList.add("light");
