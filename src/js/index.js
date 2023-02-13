3fafa5e961b8be61f89fb856d93e57abec1dafa6
Raif Coonjah <raifcoonjah@proton.me>
2023-02-13 22:47:12 +0400

    feat: Minimalistic UI
    * A new way to use Saturn, a very much beta feature with a very ugly UI.
    
    Signed-off-by: Raif Coonjah <raifcoonjah@proton.me>

--- src/js/index.js
+++ src/js/index.js
@@ -528,3 +528,34 @@ window.addEventListener(
   },
   false
 );
+
+const minimalisticButton = document.querySelector("#toggle_minimalistic_mode");
+const minimalisticUi = document.querySelector(".minimalistic-ui");
+const satHeader = document.querySelector(".sat-header");
+
+minimalisticButton.addEventListener("click", () => {
+  if (minimalisticButton.checked) {
+    minimalisticUi.style.display = "none";
+    localStorage.setItem("minimalisticUiDisplay", "none");
+    satHeader.style.paddingTop = "30vh";
+    satHeader.style.fontSize = "20px";
+  } else {
+    minimalisticUi.style.display = "block";
+    localStorage.removeItem("minimalisticUiDisplay");
+    // Applying a paddingTop of 100px to make things "appear" right. 
+    satHeader.style.paddingTop = "100px"; 
+    satHeader.style.fontSize = "0";
+    satHeader.style.marginTop = "0";
+  }
+});
+
+const savedDisplay = localStorage.getItem("minimalisticUiDisplay");
+if (savedDisplay) {
+  minimalisticUi.style.display = savedDisplay;
+  minimalisticButton.checked = savedDisplay === "none";
+
+  if (minimalisticUi.style.display === "none") {
+    satHeader.style.paddingTop = "30vh";
+    satHeader.style.fontSize = "20px";
+  }
+}
